c05c38d49ae0b322af589fb2745b0c2af631c126
Antonio Mateos <antoniomateosmariscal@gmail.com>
2024-01-27 15:19:17 +0100

    chore: Create Splash Screen

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup>
-import InitialView from '@/views/InitialView.vue';
+/* import InitialView from '@/views/InitialView.vue'; */
+import SplashScreen from './views/SplashScreen.vue';
+
+import { defineAsyncComponent } from 'vue'
+
+const InitialView = defineAsyncComponent(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(import("./views/InitialView.vue"));
+    }, 4000);
+  })
+})
 
 
 const password = "sunflower";
 </script>
 
 <template>
-  <InitialView
-    :password="password"
-  />
+  <Suspense>
+    <template #default>
+      <InitialView
+        :password="password"
+      />
+    </template>
+    <template #fallback>
+      <SplashScreen/>
+    </template>
+  </Suspense>
 </template>
 
 <style>
---
+++ src/views/SplashScreen.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="bg-light">
+    <div class="loader">
+      <img class="heart-icon" src="@/assets/icons/heart-icon.svg" alt="">
+      <img class="heart-icon" src="@/assets/icons/heart-icon.svg" alt="">
+      <img class="heart-icon" src="@/assets/icons/heart-icon.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+}
+.loader {
+  display: flex;
+  gap: var(--spacing-L);
+}
+.heart-icon {
+  height: 100px;
+  width: 100px;
+  animation: heart-animation 2s linear infinite;
+}
+.heart-icon:nth-of-type(1) {
+  animation-delay: 0;
+}
+.heart-icon:nth-of-type(2) {
+  animation-delay: .3s;
+}
+.heart-icon:nth-of-type(3) {
+  animation-delay: .6s;
+}
+
+@keyframes heart-animation {
+  0% {
+    transform: scale(1)
+  }
+  50% {
+    transform: scale(.2)
+  }
+  100% {
+    transform: scale(1)
+  }
+}
+</style>
